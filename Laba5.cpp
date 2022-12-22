97c7bc8f0b36752e11560a75f8e821f0866d2aec
Davison322 <dimakolodiy.ua@gmail.com>
2022-12-22 09:54:04 +0200

    update according to comments

--- Laba5.cpp
+++ Laba5.cpp
@@ -1,94 +1,4 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-class Planet
-{
-	
-	friend class Planetary;
-
-	private:
-		string name;
-		int massInKg;
-		int orbitalVelocity;
-		double meanTemperature;
-		double lenghtOfDay;
-		int distanceFromSun;
-	public:
-		Planet(string namee, int mass, int velocity, double temperature, double lenghtofday, int distance)
-		{
-			name = namee;
-			massInKg = mass;
-			orbitalVelocity = velocity;
-			meanTemperature = temperature;
-			lenghtOfDay = lenghtofday;
-			distanceFromSun = distance;
-		};
-};
-
-enum type
-{
-	TERRESTRIAL,
-	JOVIAN
-};
-
-class Planetary
-{
-	public:
-	vector<Planet> listOfPlanets;
-
-	public:
-
-	void listOfPlanetsAdd(Planet somePlanet)
-	{
-		listOfPlanets.push_back(somePlanet);
-	}
-
-	double findDistanceBetween(Planet firstPlanet, Planet secondPlanet)
-	{
-		cout << firstPlanet.distanceFromSun + secondPlanet.distanceFromSun << endl;
-	}
-
-	void print()
-	{
-		for (int i = 0; i < listOfPlanets.size(); i++)
-		{
-			cout << listOfPlanets[i].name << " ";
-		}
-		cout << endl;
-	}
-
-	void sortByLenghtOfDay()
-	{
-		int i, j;
-		int n = listOfPlanets.size();
-		for (int i = 0; i < n - 1; i++)
-		{
-			for (j = 0; j < n - i - 1; j++)
-			{
-				if (listOfPlanets[j].distanceFromSun > listOfPlanets[j + 1].distanceFromSun)
-				{
-					swap(listOfPlanets[j], listOfPlanets[j + 1]);
-				}
-			}
-		}
-
-	}
-
-	static double findAverageMass(vector<Planet> listOfPlanets)
-	{
-		double sum = 0;
-        for (int i = 0; i < listOfPlanets.size(); i++) {
-            sum += listOfPlanets[i].massInKg;
-        }
-		double avarege = (double)sum / listOfPlanets.size();
-        return avarege;
-	}
-
-
-};
-
-
+#include "Laba5Header.h"
 
 int main()
 {
@@ -96,14 +6,14 @@ int main()
 	Planet PlanetB("PlanetB", 1234, 88888, 12, 987654321, 400000);
 	Planetary Magnus;
 
-	Magnus.listOfPlanetsAdd(PlanetA);
-	Magnus.listOfPlanetsAdd(PlanetB);
-	Magnus.print();
+	Magnus.ListOfPlanetsAdd(PlanetA);
+	Magnus.ListOfPlanetsAdd(PlanetB);
+	Magnus.Print();
 
-	Magnus.findDistanceBetween(PlanetA, PlanetB);
+	Magnus.FindDistanceBetween(PlanetA, PlanetB);
 
-	Magnus.sortByLenghtOfDay();
-	Magnus.print();
+	Magnus.SortByLenghtOfDay();
+	Magnus.Print();
 	
 	double lasthope = Planetary::findAverageMass(Magnus.listOfPlanets);
 	cout << lasthope << endl;
